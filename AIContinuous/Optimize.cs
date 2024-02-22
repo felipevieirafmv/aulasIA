9f2d2cae807fa0202f56f93eb392c2327afcaea9
Felipe de Mello Vieira <felipevieirafmv@gmail.com>
2024-02-22 13:53:00 -0300

    Updating Gradient descent

--- AIContinuous/Optimize.cs
+++ AIContinuous/Optimize.cs
@@ -1,4 +1,5 @@
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Serialization.Formatters;
 
 namespace AIContinuous;
 
@@ -20,20 +21,27 @@ public static class Optimize
         return Root.Newton(diffFunction, diffSecondFunction, x0, atol, maxIter);
     }
 
-    public static double GradientDescent(
-        Func<double, double> function,
-        double x0,
+    public static double[] GradientDescent(
+        Func<double[], double> function,
+        double[] x0,
         double learningRate = 1e-2,
         double atol = 1e-4)
     {
-        double xp = x0;
-        double diff = Diff.Differentiate(function, xp);
+        var dim = x0.Length;
+        var xp = (double[])x0.Clone();
+        var diff = Diff.Gradient(function, xp);
+        double diffNorm;
 
-        while(Math.Abs(diff) > atol)
+        do
         {
-            diff = Diff.Differentiate(function, xp);
-            xp -= learningRate * diff;
-        }
+            diffNorm = 0.0;
+            diff = Diff.Gradient(function, xp);
+            for(int i = 0; i < dim; i++)
+            {
+                xp[i] -= learningRate * diff[i];
+                diffNorm += Math.Abs(diff[i]);
+            }
+        } while(diffNorm > atol * dim);
 
         return xp;
     }
