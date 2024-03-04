331aab7db0e4e40a357fd71265921dbee05cef84
Felipe de Mello Vieira <felipevieirafmv@gmail.com>
2024-03-04 16:56:21 -0300

    Adding Graph

---
+++ Collections/Graph.cs
@@ -0,0 +1,7 @@
+namespace AIDiscrete.Collections;
+
+public class Graph<T> : Node<T>
+{
+    public Graph(T value = default(T), List<Node<T>> neighbours = null!)
+    : base(value, neighbours) { }
+}
--- Program.cs
+++ Program.cs
@@ -1,58 +1,60 @@
 using AIDiscrete.Search;
 using AIDiscrete.Collections;
 
-var tree = BuildTree();
-Console.WriteLine(tree);
+// var tree = BuildTree();
+// Console.WriteLine(tree);
 
-var found = Search.DFSearch(tree.Root, 9);
-Console.WriteLine($"Goal found: {found}");
+// var found = Search.DFSearch(tree.Root, 9);
+// Console.WriteLine($"Goal found: {found}");
 
-found = Search.BFSearch(tree.Root, 9);
-Console.WriteLine($"Goal found: {found}");
+// found = Search.BFSearch(tree.Root, 9);
+// Console.WriteLine($"Goal found: {found}");
 
-return;
+// return;
 
 
-Tree<int> BuildTree()
-{
-    // Tree 1 (root: 50)
-    var node = new TreeNode<int>(6);
-    node = new TreeNode<int>(21, children: new List<TreeNode<int>> { node });
-    var node2 = new TreeNode<int>(45);
-    node = new TreeNode<int>(12, children: new List<TreeNode<int>> { node, node2 });
-    node = new TreeNode<int>(50, children: new List<TreeNode<int>> { node });
+// Tree<int> BuildTree()
+// {
+//     // Tree 1 (root: 50)
+//     var node = new TreeNode<int>(6);
+//     node = new TreeNode<int>(21, children: new List<TreeNode<int>> { node });
+//     var node2 = new TreeNode<int>(45);
+//     node = new TreeNode<int>(12, children: new List<TreeNode<int>> { node, node2 });
+//     node = new TreeNode<int>(50, children: new List<TreeNode<int>> { node });
 
-    var tree1 = new Tree<int>(node);
+//     var tree1 = new Tree<int>(node);
 
-    // Tree 2 (root: 1)
-    var root = new TreeNode<int>(1)
-               .AddChild(new TreeNode<int>(70))
-               .AddChild(new TreeNode<int>(61));
+//     // Tree 2 (root: 1)
+//     var root = new TreeNode<int>(1)
+//                .AddChild(new TreeNode<int>(70))
+//                .AddChild(new TreeNode<int>(61));
 
-    var tree2 = new Tree<int>(root);
+//     var tree2 = new Tree<int>(root);
 
-    // Tree 3 (root: 30)
-    root = new TreeNode<int>(30)
-           .AddChild(new TreeNode<int>(96))
-           .AddChild(new TreeNode<int>(9));
+//     // Tree 3 (root: 30)
+//     root = new TreeNode<int>(30)
+//            .AddChild(new TreeNode<int>(96))
+//            .AddChild(new TreeNode<int>(9));
 
-    var tree3 = new Tree<int>(root);
+//     var tree3 = new Tree<int>(root);
 
-    // Tree4 (root: 150)
-    root = new TreeNode<int>(150)
-           .AddChild(tree3.Root)
-           .AddChild(new TreeNode<int>(5))
-           .AddChild(new TreeNode<int>(11));
+//     // Tree4 (root: 150)
+//     root = new TreeNode<int>(150)
+//            .AddChild(tree3.Root)
+//            .AddChild(new TreeNode<int>(5))
+//            .AddChild(new TreeNode<int>(11));
 
-    var tree4 = new Tree<int>(root);
+//     var tree4 = new Tree<int>(root);
 
-    // Tree 5 (root: 100)
-    root = new TreeNode<int>(100)
-           .AddChild(tree1.Root)
-           .AddChild(tree2.Root)
-           .AddChild(tree4.Root);
+//     // Tree 5 (root: 100)
+//     root = new TreeNode<int>(100)
+//            .AddChild(tree1.Root)
+//            .AddChild(tree2.Root)
+//            .AddChild(tree4.Root);
 
-    var tree5 = new Tree<int>(root);
+//     var tree5 = new Tree<int>(root);
 
-    return tree5;
-}
+//     return tree5;
+// }
+
+var nodePortland = new Node<string>("Portland");
